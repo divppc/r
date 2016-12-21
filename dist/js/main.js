6d9360344ceeff3455b451d1cb9f20d1e39e7cd3
divppc <okal.ihor@gmail.com>
2016-12-21 18:43:03 +0200

    Bugs fixed & new page added

--- dist/js/main.js
+++ dist/js/main.js
@@ -10,18 +10,40 @@
         autoplayHoverPause: true,
         autoplaySpeed: 500,
         dots: true,
-        // controlsClass: 'main-slider',
         dotsClass: 'dots',
         dotClass: 'dot'
 	});
 
+	$(".cb-btn").on("click", function () {
+		$(".modal-bg").fadeIn(400);
+		$(".modal.cb").fadeIn(500);
+	});
 
-	// $('.left-arrow').click(function() {
-	//     slider.trigger('prev.owl.carousel');
-	// });
+	$(".leave-order").on("click", function () {
+		$(".modal-bg").fadeIn(400);
+		$(".modal.order").fadeIn(500);
+	});
 
-	// $('.right-arrow').click(function() {
-	//     slider.trigger('next.owl.carousel');
-	// });
+	$(".call-master").on("click", function (e) {
+		e.preventDefault();
+		$(".modal-bg").fadeIn(400);
+		$(".modal.master").fadeIn(500);
+	});
+
+	$(".close-btn").on("click", function () {
+		$(".modal-bg").fadeOut(400);
+		$(".modal").fadeOut(300);
+	});
+
+	$("body").on("click", function(e) {
+        if($(e.target).closest(".modal").length == 0 &&
+        	($(e.target).closest(".cb-btn").length == 0) &&
+        	($(e.target).closest(".leave-order").length == 0) &&
+        	($(e.target).closest(".call-master").length == 0)) 
+        {
+            $(".modal-bg").fadeOut(400);
+			$(".modal").fadeOut(300);
+        };        
+    });
 
 });
